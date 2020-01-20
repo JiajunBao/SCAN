1d5520f254ccf45ec3040f17190048b507e9be95
Jiajun <jiajunb@umich.edu>
2020-01-20 10:44:09 -0500

    fixed bug: index // self.im_div

--- data.py
+++ data.py
@@ -48,7 +48,7 @@ class PrecompDataset(data.Dataset):
 
     def __getitem__(self, index):
         # handle the image redundancy
-        img_id = index/self.im_div
+        img_id = index // self.im_div
         image = torch.Tensor(self.images[img_id])
         caption = self.captions[index]
         vocab = self.vocab
